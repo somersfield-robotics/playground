1ce60504ef2cc18a075fa8ce120ec22f3c64a76d
sstorey-nephila <sstorey@nephilacapital.com>
2019-03-24 13:34:46 -0300

    Create example_Autonomous.cpp

---
+++ example_Autonomous.cpp
@@ -0,0 +1,50 @@
+task main()
+{
+
+  // Pseudocode:
+  // Start with the intake facing the flags 
+
+  // Move backward 24 inches
+  driveDistance(24);
+
+  // Turn right 90 degrees
+  driveTurnRightDegrees(90);
+
+  // Move backward 36 inches 
+  driveDistance(-36);
+
+  // Flip cap
+  raiseFlipper();
+  lowereFlipper();
+
+  // Turn left 90 degrees
+  driveTurnLeftDegrees(90);
+
+  // Move forward 24 inches
+  driveDistance(24);
+
+  // Turn right 90 degrees
+  driveTurnRightDegrees(90);
+
+  // Move forward 48 inches
+  driveDistance(48);
+
+  // Turn left 90 degrees
+  driveTurnLeftDegrees(90);
+
+  // Move forward 24 inches
+  driveDistance(24);
+
+  // Shoot preload
+  shootPreload();
+
+  // Turn left 90 degrees
+  driveTurnLeftDegrees(90);
+
+  // Move forward 30 inches
+  driveDistance(30);
+
+
+
+
+}
